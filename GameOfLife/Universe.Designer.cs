f2c79d2b5ae77904c6df45b5620f3d61fc15a8ff
vova.chakalov.2003 <vova.chakalov.2003@gmail.com>
2022-08-26 20:46:58 +0300

    Добавил stop/start,и отрисовку след. поколения

--- GameOfLife/Universe.Designer.cs
+++ GameOfLife/Universe.Designer.cs
@@ -28,38 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.map = new System.Windows.Forms.PictureBox();
+            this.ResolutionLabel = new System.Windows.Forms.Label();
+            this.resolution = new System.Windows.Forms.NumericUpDown();
+            this.density = new System.Windows.Forms.NumericUpDown();
+            this.DensityLable = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.map)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resolution)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.density)).BeginInit();
             this.SuspendLayout();
             // 
-            // splitContainer1
+            // splitContainer
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Size = new System.Drawing.Size(734, 461);
-            this.splitContainer1.SplitterDistance = 181;
-            this.splitContainer1.TabIndex = 0;
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer.IsSplitterFixed = true;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.stop);
+            this.splitContainer.Panel1.Controls.Add(this.start);
+            this.splitContainer.Panel1.Controls.Add(this.density);
+            this.splitContainer.Panel1.Controls.Add(this.DensityLable);
+            this.splitContainer.Panel1.Controls.Add(this.resolution);
+            this.splitContainer.Panel1.Controls.Add(this.ResolutionLabel);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.map);
+            this.splitContainer.Size = new System.Drawing.Size(734, 461);
+            this.splitContainer.SplitterDistance = 181;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // map
+            // 
+            this.map.BackColor = System.Drawing.Color.IndianRed;
+            this.map.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.map.Location = new System.Drawing.Point(0, 0);
+            this.map.Name = "map";
+            this.map.Size = new System.Drawing.Size(549, 461);
+            this.map.TabIndex = 0;
+            this.map.TabStop = false;
+            // 
+            // ResolutionLabel
+            // 
+            this.ResolutionLabel.AutoSize = true;
+            this.ResolutionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ResolutionLabel.Location = new System.Drawing.Point(17, 23);
+            this.ResolutionLabel.Name = "ResolutionLabel";
+            this.ResolutionLabel.Size = new System.Drawing.Size(85, 20);
+            this.ResolutionLabel.TabIndex = 0;
+            this.ResolutionLabel.Text = "Resolution";
+            // 
+            // resolution
+            // 
+            this.resolution.Location = new System.Drawing.Point(21, 59);
+            this.resolution.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.resolution.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.resolution.Name = "resolution";
+            this.resolution.Size = new System.Drawing.Size(120, 20);
+            this.resolution.TabIndex = 1;
+            this.resolution.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.resolution.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // density
+            // 
+            this.density.Location = new System.Drawing.Point(21, 136);
+            this.density.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.density.Name = "density";
+            this.density.Size = new System.Drawing.Size(120, 20);
+            this.density.TabIndex = 3;
+            this.density.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.density.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // DensityLable
+            // 
+            this.DensityLable.AutoSize = true;
+            this.DensityLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DensityLable.Location = new System.Drawing.Point(17, 92);
+            this.DensityLable.Name = "DensityLable";
+            this.DensityLable.Size = new System.Drawing.Size(62, 20);
+            this.DensityLable.TabIndex = 2;
+            this.DensityLable.Text = "Density";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(21, 175);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(64, 34);
+            this.start.TabIndex = 4;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(91, 175);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(64, 34);
+            this.stop.TabIndex = 5;
+            this.stop.Text = "Stop";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 40;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // Universe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(734, 461);
-            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.splitContainer);
             this.MinimumSize = new System.Drawing.Size(500, 500);
             this.Name = "Universe";
             this.Text = "Universe";
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel1.PerformLayout();
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.map)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resolution)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.density)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.PictureBox map;
+        private System.Windows.Forms.NumericUpDown resolution;
+        private System.Windows.Forms.Label ResolutionLabel;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.NumericUpDown density;
+        private System.Windows.Forms.Label DensityLable;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- GameOfLife/Universe.cs
+++ GameOfLife/Universe.cs
@@ -12,10 +12,95 @@ namespace GameOfLife
 {
     public partial class Universe : Form
     {
+        private Graphics graphics;
+        private int _resolution;
+        private int _density;
+        private bool[,] field;
+        private int rows;
+        private int cols;
         public Universe()
         {
             InitializeComponent();
             StartPosition= FormStartPosition.CenterScreen;
+        }
+
+        private void StartGame()
+        {
+            if (timer.Enabled)
+                return;
+
+            resolution.Enabled = false;
+            density.Enabled = false;
+
+            _resolution = (int)resolution.Value;
+            _density= (int)density.Value;
+
+            rows = map.Height / _resolution;
+            cols = map.Width / _resolution;
+            field = new bool[cols, rows];
+            Random random = new Random();
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next(_density)==0 ;
+                }
+            }
+
+            map.Image = new Bitmap(map.Width, map.Height);
+            graphics = Graphics.FromImage(map.Image);
+            timer.Start();
+            //graphics.FillRectangle(Brushes.Blue, 0, 0, _resolution, _resolution);
+        }
+
+        private void NextGeneration()
+        {
+            graphics.Clear(Color.IndianRed);
+
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    if (field[x, y])
+                    {
+                        graphics.FillRectangle(Brushes.Blue,x*_resolution,y*_resolution,_resolution,_resolution);
+                    }
+                }
+            }
+            map.Refresh();
+            Random random = new Random();
+            for (int x = 0; x < cols; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    field[x, y] = random.Next(_density) == 0;
+                }
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+           NextGeneration();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+           StartGame();
+        }
+
+        private void StopGame()
+        {
+            if(!timer.Enabled)
+                return;
+            timer.Stop();
+
+            resolution.Enabled = true;
+            density.Enabled = true;
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            StopGame();
         }
     }
 }
